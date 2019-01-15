a3684fe0e0e6a1984875be16569e487960bb31b4
Paul  d'Hubert <paul@tymate.com>
2019-01-15 11:08:07 +0100

    feat: enable sticky windows

--- config.def.h
+++ config.def.h
@@ -1,22 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "terminus:size=10" };
 static const char dmenufont[]       = "terminus:size=10";
-static const char col_gray1[]       = "#1e1b19";
-static const char col_gray2[]       = "#FF875F";
+static const char col_gray1[]       = "#181818";
+static const char col_orange[]      = "#FF875F";
 static const char col_red[]       	= "#C50610";
 static const char col_gray4[]       = "#ABB2BF";
-static const char col_cyan[]        = "#6E88A6";
+static const char col_cyan[]        = "#29AEE1";
+static const char col_white[]       = "#F4E2DD";
 static const char col_purple[]      = "#24121C";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_red, col_gray1, col_purple },
-	[SchemeSel]  = { col_purple, col_gray1,  col_gray2  },
+	[SchemeNorm] = { col_white, col_gray1, col_purple },
+	[SchemeSel]  = { col_red, col_gray1,  col_red  },
 };
 
 /* tagging */
@@ -66,7 +67,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_white, "-sb", col_red, "-sf", col_white, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
@@ -90,6 +91,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+ 	{ MODKEY,                       XK_s,      togglesticky,   {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
